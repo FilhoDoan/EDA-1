f3e06f19b0abd71b2ca66c176df493de97eb8ab3
FilhoDoan <doangalvaofilho@gmail.com>
2023-04-13 08:50:22 -0300

    Questão Manhattan

---
+++ Lista1/Manhattan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// ruas paralelas ou perpendiculares 
+//definir os cruzamentos 
+
+int main(){
+
+    //xm e ym as coordenadas de Maria
+    //xr e yr as coordenadas da Reuniao 
+    // o ponto de partida nunca sera igual ao local da reuniao 
+    int xm, ym, xr,yr;
+
+    scanf("%d %d %d %d ", &xm, &ym, &xr, &yr);
+
+    if(0<= xm && ym <= 1000 && 0<= xr && yr <= 1000 ){
+
+        
+
+
+
+    }
+
+
+
+}
