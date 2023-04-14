4a4dcbb5abadba2d11d758436af7c15a613a3d19
Doan Filho <doangalvaofilho@gmail.com>
2023-04-14 20:20:10 -0300

    Manhattan atualizado

--- Lista1/Manhattan.c
+++ Lista1/Manhattan.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 // ruas paralelas ou perpendiculares 
-//definir os cruzamentos 
+//definir os cruzamentos
+ 
 
 int main(){
-
-    //xm e ym as coordenadas de Maria
-    //xr e yr as coordenadas da Reuniao 
-    // o ponto de partida nunca sera igual ao local da reuniao 
+     
     int xm, ym, xr,yr;
+    int cruzamentos; 
 
-    scanf("%d %d %d %d ", &xm, &ym, &xr, &yr);
+    scanf("%d %d %d %d", &xm, &ym, &xr, &yr);
+    
+    cruzamentos = sqrt(pow((xr - xm),2.0) + pow((yr - ym),2.0));
 
-    if(0<= xm && ym <= 1000 && 0<= xr && yr <= 1000 ){
-
-        
-
-
-
-    }
+    printf("%d", cruzamentos);
 
 
 
