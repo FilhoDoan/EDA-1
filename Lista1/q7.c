5b7f7404fe1c95d3cc372ed989a3a75c008693dc
Doan Filho <doangalvaofilho@gmail.com>
2023-04-19 15:23:51 -0300

    Questão 7

---
+++ Lista1/q7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main (){
+    /*   A = preço do Alcool 
+         G = preco da Gasolina 
+         Ra = rendimento por km do alcool
+         Rg = rendimento pr km de Gasolina
+    */
+
+
+
+    double a,g,ra,rg;
+
+    scanf("%lf %lf %lf %lf", &a, &g,&ra,&rg);
+
+
+     
+
+    if(){
+        printf("G");
+    }else if (){
+        printf("A");
+    }
+    
+
+
+   
+}
