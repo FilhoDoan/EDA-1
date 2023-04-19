f3b58ad4e7ad3affdbbe33510fef30b882545862
FilhoDoan <doangalvaofilho@gmail.com>
2023-04-19 16:12:44 -0300

    Questao & (taxi) resolvido

--- Lista1/q7.c
+++ Lista1/q7.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
-
-int main (){
-    /*   A = preço do Alcool 
-         G = preco da Gasolina 
+int main()
+{
+    /*   A = preço do Alcool
+         G = preco da Gasolina
          Ra = rendimento por km do alcool
          Rg = rendimento pr km de Gasolina
+
+         CustoA = custo rendido em relação ao preco do Alcool e
+        rendimento por km dando o custo por km
+         CustoG = custo rendido em relação ao preco do Gasolina e
+        rendimento por km dando o custo por km
+
+         os custos são calculados como a divisão do preço pelo rendimento
+
     */
 
+    double a, g, ra, rg, custoA, custoG;
 
+    scanf("%lf %lf %lf %lf", &a, &g, &ra, &rg);
 
-    double a,g,ra,rg;
+    custoA = a / ra;
+    custoG = g / rg;
 
-    scanf("%lf %lf %lf %lf", &a, &g,&ra,&rg);
-
-
-     
-
-    if(){
+    if (custoA >= custoG)
+    {
         printf("G");
-    }else if (){
+    }
+    else
+    {
         printf("A");
     }
-    
-
-
-   
 }
