08d82051cd40ae5be8046c2d5c786e383795d00a
Doan Filho <doangalvaofilho@gmail.com>
2023-05-07 00:20:46 -0300

    Elevador pronto

---
+++ Lista1/q9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main (){
+    //atributos, tendo algumas variaveis para ajudar a fazer os calculos    
+   int num_Leituras, capacidade_Max, saidas = 0, entradas = 0, cont_Entr, cont_Sai, alerta = 0 ;
+         
+        scanf("%d %d", &num_Leituras ,&capacidade_Max);
+
+        // for com limitante relacionado ao numero de leituras 
+        for(int i = 0; i < num_Leituras; i++){
+        
+            scanf("%d %d",&cont_Sai, & cont_Entr);
+            
+            entradas -= cont_Sai; 
+            entradas += cont_Entr;
+            
+            if(entradas > capacidade_Max){
+                alerta ++;
+            }
+
+    } printf("%d", alerta);
+     if(alerta > 0){
+        printf("S");
+     }else{
+     printf("N");
+     }         
+}
+   
